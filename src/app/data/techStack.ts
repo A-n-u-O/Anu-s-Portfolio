10394ea54bee7af2e215545e84d15c80dd23b9c1
Anuoluwapo <anuvictor2005@gmail.com>
2025-09-16 11:30:15 +0100

    skill icons added

--- src/app/data/techStack.ts
+++ src/app/data/techStack.ts
@@ -14,19 +14,29 @@ export const techStack = [
       { name: "React", icon: "/assets/icons/react-svgrepo-com.svg" },
       { name: "Next.js", icon: "/assets/icons/nextjs-icon.svg" },
       { name: "Tailwind CSS", icon: "/assets/icons/tailwindcss-icon-svgrepo-com.svg" },
+      { name: "Framer Motion", icon: "/assets/icons/framer-motion.svg" },
+      // { name: "Anime.js", icon: "/assets/icons/animejs.png" },
     ],
   },
   {
     category: "Backend & State",
     items: [
       { name: "Node.js", icon: "/assets/icons/Node.js.svg" },
-      { name: "Zustand", icon: "/assets/icons/zustand.png" },
+      { name: "Express.js", icon: "/assets/icons/expressjs.svg" },
+      { name: "Zustand", icon: "/assets/icons/zustand.svg" },
+      { name: "Supabase", icon: "/assets/icons/supabase-icon.svg" },
+      { name: "PostgreSQL", icon: "/assets/icons/postgresql.svg" },
+      { name: "MySQL", icon: "/assets/icons/mysql.svg" },
     ],
   },
   {
-    category: "Tools",
+    category: "Tools & Platforms",
     items: [
       { name: "Git", icon: "/assets/icons/git.svg" },
+      { name: "GitHub", icon: "/assets/icons/github.svg" },
+      { name: "Vercel", icon: "/assets/icons/vercel.svg" },
+      { name: "Figma", icon: "/assets/icons/figma.svg" },
+      { name: "Visual Studio Code", icon: "/assets/icons/vscode.svg" },
     ],
   },
 ];
