a26a44fde347f19bcf41f539844024ec440b8ad7
Anuoluwapo <anuvictor2005@gmail.com>
2025-10-29 12:21:03 +0100

    project section bug fixed

--- src/components/ShowCase.tsx
+++ src/components/ShowCase.tsx
@@ -5,18 +5,14 @@ import Projects from "@/components/Project";
 
 export default function Showcase() {
   return (
-    // <motion.section
-    //   id="showcase"
-    //   initial={{ opacity: 0, y: 40 }}
-    //   whileInView={{ opacity: 1, y: 0 }}
-    //   transition={{ duration: 0.8, ease: "easeOut" }}
-    //   viewport={{ once: true, amount: 0.2 }}
-    //   className="bg-[#0b0b0b] text-white px-4 sm:px-6 md:px-12 lg:px-16 py-20 sm:py-24 md:py-28 min-h-screen"
-    // >
-      <div className="bg-[#0b0b0b] pt-14 text-white min-w-6xl mx-auto text-center">
+    <section
+      id="showcase"
+      className="bg-[#0b0b0b] text-white px-4 sm:px-6 md:px-12 lg:px-16 py-16 sm:py-24"
+    >
+      <div className="max-w-6xl mx-auto text-center">
         {/* Heading */}
         <motion.h2
-          className="text-3xl mt-8 sm:text-4xl md:text-6xl font-bold bg-gradient-to-r from-blue-400 via-purple-400 to-pink-400 bg-clip-text text-transparent "
+          className="text-3xl sm:text-4xl md:text-6xl font-bold bg-gradient-to-r from-blue-400 via-purple-400 to-pink-400 bg-clip-text text-transparent"
           initial={{ opacity: 0, y: 20 }}
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.6 }}
@@ -25,10 +21,10 @@ export default function Showcase() {
         </motion.h2>
 
         {/* Projects Section */}
-        <div className="mt-2">
+        <div className="mt-10">
           <Projects />
         </div>
       </div>
-    // </motion.section>
+    </section>
   );
 }
