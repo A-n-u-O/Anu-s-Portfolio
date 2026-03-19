d5fdbd746968f9041de16ae801fceb9232e8e92b
Anuoluwapo <121236333+A-n-u-O@users.noreply.github.com>
2026-03-19 07:36:26 +0100

    update: tech events updated

--- src/app/data/techEvents.ts
+++ src/app/data/techEvents.ts
@@ -26,6 +26,24 @@ export const events: eventDetails[] = [
     description:
       "Volunteered as part of the organizing committee for TEDxBabcock University, supporting logistics, planning, and coordination of the event. Gained hands-on experience in event organization, teamwork, and community engagement.",
     skills: ["Event Coordination", "Teamwork", "Communication"],
-    // link: "https://www.linkedin.com/posts/a-n-u-o_tedxbabcock-innovation-ideasworthspreading-activity-7177289115329837056-XYzO?utm_source=share&utm_medium=member_desktop",
   },
+  {
+    title: "OnePipe Cortex 3.0 Fintech Accelerator",
+    organizer: "OnePipe",
+    date: "November 2025 - February 2026",
+    description:
+      "Selected among the top participants in the OnePipe Cortex 3.0 Fintech Accelerator. Collaborated with a team to build fintech-focused solutions, gaining hands-on experience in product development, teamwork, and fintech innovation.",
+    skills: ["Fintech", "Product Development", "Team Collaboration"],
+    link: "https://www.linkedin.com/posts/a-n-u-o_cortex3-fintech-openbanking-share-7440049391278800896-yUU8?utm_source=share&utm_medium=member_desktop&rcm=ACoAAD6lPY4BSGMOc-9xDhCh1Gq3CY4QfPPXHu8"
+  },
+  {
+    title: "She Code Africa Mentorship Program – Cycle 3",
+    organizer: "She Code Africa",
+    date: "March2026",
+    description:
+      "Participated in the She Code Africa Mentorship Program (Cycle 3), where I received structured mentorship, career guidance, and technical learning support while connecting with a community of women in technology.",
+    skills: ["Mentorship", "Community", "Career Development", "Frontend Development"],
+    // link: ""
+  },
+  
 ];
